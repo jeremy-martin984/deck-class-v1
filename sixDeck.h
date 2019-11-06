0cbacb45b12bcae69faed4c640d6d4728ab99f69
jeremy-martin984 <55176671+jeremy-martin984@users.noreply.github.com>
2019-11-05 23:31:45 -0600

    Add files via upload

---
+++ sixDeck.h
@@ -0,0 +1,50 @@
+#pragma once
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<ctime>
+#include<cstdlib>
+/*
+function list
+class.shuffle() shuffles the deck, resets draw counter to -1
+class.setDrawNumber() sets deck position
+class.getDrawNumber() returns deck position
+class.draw() returns card at current deck position and increments draw counter by one
+*/
+	using namespace std;
+	class sixDeck
+	{
+	private:
+		int drawNumber = -1;
+		int cardNumber[312] =
+		{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+			 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+			 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+			 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+			 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+			 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+		}; //52 cards per deck, 6 decks; add or remove in increments of 52 to change deck size, update array element size
+	public:
+		void shuffle()
+		{
+			srand(unsigned(time(0)));
+			random_shuffle(&cardNumber[0], &cardNumber[312]);
+			drawNumber = -1;
+
+		}
+		void setDrawNumber(int d)
+		{
+			drawNumber = d;
+		}
+		int draw()
+		{
+			drawNumber++;
+			return cardNumber[drawNumber];
+			
+		}
+		int getDrawNumber()
+		{
+			return drawNumber;
+		}
+	};
+
